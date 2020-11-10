d43b79dde85bc0c9eee40eede762c8792d5cae98
Filip <filip.wozniak99@gmail.com>
2020-11-10 21:07:20 +0100

    zad2/test_at_least_8_letters/green

--- zad2/src/validpassword.py
+++ zad2/src/validpassword.py
@@ -1,20 +1,26 @@
 from string import ascii_letters, digits
 import string
 
+
 class Password():
     def ValidPassword(self, password):
+        lettersNum = 0
         if len(password) < 10:
             return False
         else:
             if set(password).difference(ascii_letters + digits):
                 if set(password).difference(ascii_letters + string.punctuation):
-                    return True
+                    for letter in password:
+                        if letter not in digits+string.punctuation:
+                            lettersNum = lettersNum + 1
+                    if lettersNum>=8:
+                        return True
+                    else:
+                        return False
                 else:
                     return False
             else:
                 return False
-
-
 
 if __name__ == "__main__":
     import doctest
