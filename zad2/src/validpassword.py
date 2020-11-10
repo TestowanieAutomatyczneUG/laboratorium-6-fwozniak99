6f778e907336ac6a1b8a0d47b0df15c9dadcc3d6
Filip <filip.wozniak99@gmail.com>
2020-11-10 20:36:58 +0100

    zad2/test_no_special/green

--- zad2/src/validpassword.py
+++ zad2/src/validpassword.py
@@ -1,9 +1,15 @@
+from string import ascii_letters, digits
+
 class Password():
     def ValidPassword(self, password):
         if len(password) < 10:
             return False
         else:
-            return True
+            if set(password).difference(ascii_letters + digits):
+                return True
+            else:
+                return False
+
 
 if __name__ == "__main__":
     import doctest
