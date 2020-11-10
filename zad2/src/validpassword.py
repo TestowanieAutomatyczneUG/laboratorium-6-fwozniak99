51e90494280b9afb8dffcd3eec642860f4c234da
Filip <filip.wozniak99@gmail.com>
2020-11-10 21:26:44 +0100

    zad2/doctests

--- zad2/src/validpassword.py
+++ zad2/src/validpassword.py
@@ -3,6 +3,28 @@ import string
 
 
 class Password():
+    """Check if given password is correct
+    >>> c = Password()
+    >>> c.ValidPassword("abc")
+    False
+    >>> c.ValidPassword("Password9$")
+    True
+    >>> c.ValidPassword("Password99")
+    False
+    >>> c.ValidPassword("Password&&")
+    False
+    >>> c.ValidPassword("Passw667&&")
+    False
+    >>> c.ValidPassword("password9$")
+    False
+    >>> c.ValidPassword(99)
+    Traceback (most recent call last):
+      File "C:/Users/filip/OneDrive/Pulpit/GITTEST/laboratorium-6-fwozniak99/zad2/src/validpassword.py", line 54, in <module>
+        c.ValidPassword(99)
+      File "C:/Users/filip/OneDrive/Pulpit/GITTEST/laboratorium-6-fwozniak99/zad2/src/validpassword.py", line 26, in ValidPassword
+        raise TypeError("Must be a string")
+    TypeError: Must be a string
+    """
     def ValidPassword(self, password):
         lettersNum = 0
         upperLettersNum = 0
