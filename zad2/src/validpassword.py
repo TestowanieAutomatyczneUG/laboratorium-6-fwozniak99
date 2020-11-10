ddb279280625b3bc401c192603a724e8fd13995e
Filip <filip.wozniak99@gmail.com>
2020-11-10 20:44:49 +0100

    zad2/test_no_number/green

--- zad2/src/validpassword.py
+++ zad2/src/validpassword.py
@@ -7,7 +7,10 @@ class Password():
             return False
         else:
             if set(password).difference(ascii_letters + digits):
-                return True
+                if set(password).difference(ascii_letters + string.punctuation):
+                    return True
+                else:
+                    return False
             else:
                 return False
 
