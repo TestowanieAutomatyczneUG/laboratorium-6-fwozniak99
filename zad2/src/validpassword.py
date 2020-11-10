c55b66e8d19ff0d3f570cd9e5d3241c0ff215757
Filip <filip.wozniak99@gmail.com>
2020-11-10 20:20:00 +0100

    zad2/test_correct_password/green

--- zad2/src/validpassword.py
+++ zad2/src/validpassword.py
@@ -2,6 +2,8 @@ class Password():
     def ValidPassword(self, password):
         if len(password) < 10:
             return False
+        else:
+            return True
 
 if __name__ == "__main__":
     import doctest
